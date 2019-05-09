9129403926373754fb21824c47ba0a788302303e
Gavin1997 <15108466849@163.com>
2019-05-09 11:15:01 +0800

    chore(cli): 安装cz-customizable

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: ["cz"],
   rules: {
-      'type-empty': [2, 'never'],
-      'scope-enum': [
-    0
-  ]
+    'type-empty': [2, 'never'],
+    'scope-enum': [
+      0
+    ]
   }
 };
